cedcfc693f51a373fbf1a8869d5391333386fbd1
Marian Fernando <mdfernando3@myseneca.ca>
2025-05-23 00:31:48 -0400

    SAdded lab2g

---
+++ lab2g.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+#Author: Marian Derlina Fernando
+#Author ID: mdfernando3
+#Date Created: 2025/05/23
+
+import sys
+
+if len(sys.argv) < 2:
+    timer = 3
+else:
+    timer = int(sys.argv[1])
+while timer !=0:
+    print(timer)
+    timer =  timer - 1
+print ('blast off!')
+
