b1ad3788be6f8bae57dfb789ef9067b2ffbeda6c
Marian Fernando <mdfernando3@myseneca.ca>
2025-05-23 00:22:29 -0400

    Added lab2f

---
+++ lab2f.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+#Author: Marian Derlina Fernando
+#Author ID: mdfernando3
+#Date Created: 2025/05/23
+
+import sys 
+
+timer = int(sys.argv[1])
+while timer !=0:
+    print(timer)
+    timer =  timer - 1
+print ('blast off!')
+
