7ea71ac72b6046616241fff1f69595e8e0b7a468
Marian Fernando <mdfernando3@myseneca.ca>
2025-05-23 00:13:32 -0400

    Added lab2e

---
+++ lab2e.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+#Author: Marian Derlina Fernando
+#Author ID: mdfernando3
+#Date Created: 2025/05/23
+
+timer = 10
+while timer !=0:
+    print(timer)
+    timer =  timer - 1
+print ('blast off!')
